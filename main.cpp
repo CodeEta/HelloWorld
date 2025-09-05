fc75ef3fee67e78be7be03385e072a9cb715929d
CodeEta <etacode@qq.com>
2025-09-05 11:10:53 +0800

    更新了sort函数

--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
-, #include <iostream>
-, 
+#include <iostream>
+
 using namespace std;
 
 void sort(int arr[], int size)
 {
-    
+    for (int gap = size / 2; gap > 0; gap = gap / 2)
+	{
+		for (int i = gap; i < size; i++)
+		{
+			int val = arr[i];
+			int j = i - gap;
+			for (; j >= 0; j-=gap)
+			{
+				if (arr[j] <= val)
+				{
+					break;
+				}
+				arr[j + gap] = arr[j];
+			}
+			arr[j + gap] = val;
+		}
+	}
 }
 
 int main()
