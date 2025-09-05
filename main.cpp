50f0732bfa7439d89ed6621f3b47f705ceb7d920
CodeEta <etacode@qq.com>
2025-09-05 10:48:51 +0800

    创建main.cpp文件，写了初始的代码

---
+++ main.cpp
@@ -0,0 +1,16 @@
+, #include <iostream>
+, 
+using namespace std;
+
+void sort(int arr[], int size)
+{
+    
+}
+
+int main()
+{
+    int arr[] = {6, 54, 12, 89, 75, 122, 366, 1026}
+    int size = sizeof(arr)/sizeof(arr[0]);
+    sort(arr, size);
+    return 0;, 
+}
